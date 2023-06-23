eee3575127eb5f4536242ee1bdceb9159caf3f04
bentman <26147320+bentman@users.noreply.github.com>
2023-06-22 21:53:23 -0500

    Update Use-TsToExcel.ps1

--- Use-TsToExcel.ps1
+++ Use-TsToExcel.ps1
@@ -49,7 +49,7 @@
     document at "C:\temp\TS.xlsx". The Excel document will not be displayed after it is created.
 
 .CREDITS
-    I used OpenAI's ChatGPT to refactor the original script
+    I used [OpenAI's ChatGPT](https://chat.openai.com/) to refactor the original script
     - The original script can be found at [n0spaces - Export-TSToExcel](https://github.com/n0spaces/Export-TSToExcel/tree/main)
         - Copyright (c) 2021 Matt Schwartz @ [n0spaces](https://github.com/n0spaces)
         - Implemented the core functionality of the script
